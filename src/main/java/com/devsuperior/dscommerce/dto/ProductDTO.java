5d07dfa568c0f1d8cc1fe394360fbdf2f90775e5
Camille de Oliveira <camilleoliveira324@gmail.com>
2025-05-30 14:53:46 -0300

    GET Product by Id

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Product;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    public ProductDTO() {}
+
+    public ProductDTO(Product p) {
+        id = p.getId();
+        name = p.getName();
+        description = p.getDescription();
+        price = p.getPrice();
+        imgUrl = p.getImgUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
